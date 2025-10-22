70da2288fb940470b1334910d7f895729aa0a911
Johannes Flodin <jannis020609@gmail.com>
2025-10-22 12:31:53 +0200

    added frame, text and enum structure

---
+++ Utility/Components/Frame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_app.Utility.Components
+{
+    public class Frame : UIComponent
+    {
+        List<UIComponent> Components {  get; set; }
+
+        Frame(List<UIComponent> components, Justify justify = Justify.Start, Align align = Align.Start, OrderBy orderBy = OrderBy.Row)
+        {
+            Components = components;
+        }
+    }
+}
---
+++ Utility/Enum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_app.Utility
+{
+    public enum Justify
+    {
+        Start,
+        Center,
+        End
+    }
+
+    public enum Align
+    {
+        Start,
+        Center,
+        End
+    }
+
+    public enum OrderBy
+    {
+        Row,
+        Column
+    }
+
+}
