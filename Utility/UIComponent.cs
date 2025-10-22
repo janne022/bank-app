1174b227d9e57f90cc60d48a314c07217523a500
Johannes Flodin <jannis020609@gmail.com>
2025-10-22 12:17:53 +0200

    basic structure for component library

---
+++ Utility/UIComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_app.Utility
+{
+    public class UIComponent
+    {
+        // Internal coordinates
+        public int X {  get; set; }
+        public int Y { get; set; }
+
+        public string Name { get; set; }
+
+        // Renders the component
+        public abstract void Render();
+        // Should run when user clicks component
+        public virtual void OnEnter();
+    }
+}
