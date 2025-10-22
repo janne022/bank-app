30f14bc3e5d4b63107d7217353903431c6e99cb4
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-10-22 14:10:22 +0200

    Added usertype Enum. Set up an initial version of CreateUser method

--- Managers/UserManager.cs
+++ Managers/UserManager.cs
@@ -7,8 +7,38 @@ using bank_app.Models.Users;
 
 namespace bank_app.Managers
 {
-    public class UserManager
+    public enum UserType
     {
+        Admin,
+        Client
+    }
 
+    public static class UserManager
+    {
+        public static List<User> Users { get; set; } = new List<User>();
+
+        public static void CreateUser(UserType userType, string userId, string userName, string userPassword)
+        {
+            if (userType == UserType.Admin)
+            {
+                var admin = new Admin(userId, userName, userPassword);
+                Users.Add(admin);
+            }
+            else if (userType == UserType.Client)
+            {
+                var client = new Client(userId, userName, userPassword);
+                Users.Add(client);
+            }
+        }
+
+        public static void RemoveClient()
+        {
+
+        }
+
+        public static void ChangeClient()
+        {
+
+        }
     }
 }
