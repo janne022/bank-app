a3b7571179ea7b9d6e874ddb1c84e36626c06018
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-10-23 11:27:29 +0200

    added CurrentUserType to separate Clients and Admins through enum values

--- Models/Users/User.cs
+++ Models/Users/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bank_app.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace bank_app.Models.Users
         public Guid UserId { get; set; }  = Guid.NewGuid();
         public string? UserName { get; set; }
         public string? UserPassword { get; set; }
+
+        private UserType userType {  get; set; }
 
         protected User( string userName, string userPassword)
         {
