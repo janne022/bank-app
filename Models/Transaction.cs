b3306961e38afa24cd68615fed4bad2466e21d7f
theo.lillieskold@gmail.com <theo.lillieskold@gmail.com>
2025-10-23 12:14:04 +0200

    Added transaction timestamp

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -13,10 +13,12 @@ namespace bank_app.Models
         public Account Sender { get; set; }
         public Account Reciever { get; set; }
         public decimal TransferAmount { get; set; }
+        public DateTime TimeStamp { get; set; }
 
         public Transaction(Account sender, Account reciever, decimal transferAmount)
         {
             TransactionId = CreateTransactionId();
+            TimeStamp = DateTime.Now;
             Sender = sender;
             Reciever = reciever;
             TransferAmount = transferAmount;
