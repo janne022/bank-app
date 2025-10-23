fc02b486ace1f0246320f5c1102ddc2cd661076b
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-10-23 11:47:50 +0200

    Updated CreateUser to include currentUserType property

--- Managers/UserManager.cs
+++ Managers/UserManager.cs
@@ -16,16 +16,16 @@ namespace bank_app.Managers
         /// Method to create new users within the bank app. Adds the newly created user to a list of all users within the application.
         /// </summary>
         /// <param name="userType">Enum that determines whether the user is a client or an administrator</param>
-        public static void CreateUser(UserType userType, string userId, string userName, string userPassword)
+        public static void CreateUser(UserType userType, string userId, string userName, string userPassword, string email, string phoneNumber)
         {
             if (userType == UserType.Admin)
             {
-                var admin = new Admin(userName, userPassword);
+                var admin = new Admin(userName, userPassword, email, phoneNumber, userType);
                 Users.Add(admin);
             }
             else if (userType == UserType.Client)
             {
-                var client = new Client(userName, userPassword);
+                var client = new Client(userName, userPassword, email, phoneNumber, userType);
                 Users.Add(client);
             }
         }
