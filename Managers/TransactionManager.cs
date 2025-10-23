73fa4bd59c6b39d6f4fa6aa9eea421735ff9df5c
theo.lillieskold@gmail.com <theo.lillieskold@gmail.com>
2025-10-23 15:08:33 +0100

    Create base transaction method with minimal exception handling

--- Managers/TransactionManager.cs
+++ Managers/TransactionManager.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using bank_app.Models;
+using bank_app.Models.Accounts;
+using bank_app.Utility;
 
 namespace bank_app.Managers
 {
     public class TransactionManager
     {
+        public Transaction Transfer(Account sender, Account receiver, decimal amount)
+        {
+            var transaction = new Transaction(sender, receiver, amount)
+            {
+                Status = TransferStatus.Pending
+            };
 
+            try
+            {
+                //Fails transaction if amount is greater than account balance
+                if (!sender.CanWithdraw(amount))
+                {
+                    transaction.Status = TransferStatus.Failed;
+                    return transaction;
+                }
+
+                sender.Withdraw(amount);
+                receiver.Deposit(amount);
+                transaction.Status = TransferStatus.Completed;
+                sender.AddTransaction(transaction);
+                receiver.AddTransaction(transaction);
+            }
+
+            catch (Exception)
+            {
+                transaction.Status = TransferStatus.Failed;
+                throw;
+            }
+
+            return transaction;
+        }
     }
 }
