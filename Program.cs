5a173a89ea9a6988d27c769bde77206fb3addc87
lskld <theo.lillieskold@gmail.com>
2025-10-28 17:56:40 +0100

    Created periodic timer in main method that executes transactions

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using bank_app.Managers;
+internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        
+        var timer = new PeriodicTimer(TimeSpan.FromMinutes(15));
+
+        while (await timer.WaitForNextTickAsync())
+        {
+            TransactionManager.ProcessPendingTransactions();
+        }
     }
 }
