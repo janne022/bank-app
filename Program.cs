dc662a9bdbab4b624e570c4a0d53732cf1d34f80
Johannes Flodin <jannis020609@gmail.com>
2025-10-29 12:45:15 +0100

    created a new method inside main to allow for a BackgroundThread to run the timer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿using bank_app.Managers;
 internal class Program
 {
-    private static async Task Main(string[] args)
+    private static void Main(string[] args)
+    {
+        // Starts the async method inside a background thread that needs to run without getting blocked from ex. UI
+        Task.Run(BackgroundThread);
+    }
+    private static async Task BackgroundThread()
     {
         var timer = new PeriodicTimer(TimeSpan.FromMinutes(15));
 
