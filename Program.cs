a0fb121c7c86ac3ba5b2fc51684489fc9f63ae47
Johannes Flodin <jannis020609@gmail.com>
2025-10-29 17:32:35 +0100

    changed order inside program.cs

--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Starts the async method inside a background thread that needs to run without getting blocked from ex. UI
-        Task.Run(BackgroundThread);
-    }
-    private static async Task BackgroundThread()
-    {
         // Set the console to use UTF8 encoding to allow for more colors and characters
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.InputEncoding = System.Text.Encoding.UTF8;
+        // Starts the async method inside a background thread that needs to run without getting blocked from ex. UI
+        Task.Run(BackgroundThread);
+
+        // Start the first User Interface page
         Login.Menu();
+    }
+    private static async Task BackgroundThread()
+    {
         var timer = new PeriodicTimer(TimeSpan.FromMinutes(15));
 
         while (await timer.WaitForNextTickAsync())
