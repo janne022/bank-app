35f0f348678a24d6b77659101839a031e89dd4da
lskld <theo.lillieskold@gmail.com>
2025-10-29 16:08:16 +0100

    Cleanup code  in Transaction class

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -11,32 +11,20 @@ namespace bank_app.Models
 {
     public class Transaction
     {
-        public TransactionType TransactionType { get; internal set; }
         public Guid TransactionId = Guid.NewGuid();
+        public DateTime TimeStamp = DateTime.Now;
         public Account Sender { get; set; }
         public Account Receiver { get; set; }
         public decimal TransferAmount { get; set; }
         public Currency TransferCurrency { get; set; }
-        public DateTime TimeStamp { get; set; }
         public TransferStatus Status { get; set; }
+        public TransactionType TransactionType { get; internal set; }
 
         public Transaction(Account sender, Account receiver, decimal transferAmount)
         {
-            TimeStamp = DateTime.Now;
             Sender = sender;
             Receiver = receiver;
             TransferAmount = transferAmount;
-        }
-
-        //Overloaded constructor for other currencies. Feature not implemented yet. 
-        public Transaction(Account sender, Account receiver, decimal transferAmount, Currency transferCurrency)
-        {
-            TransactionId = Guid.NewGuid();
-            TimeStamp = DateTime.Now;
-            Sender = sender;
-            Receiver = receiver;
-            TransferAmount = transferAmount;
-            TransferCurrency = transferCurrency;
         }
     }
 }
