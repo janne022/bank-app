715756977b733bd3f35f7703164a4a7231cae694
Johannes Flodin <jannis020609@gmail.com>
2025-10-30 12:48:52 +0100

    changed button to use Invokable instead for better performance and safety

--- UI/Login.cs
+++ UI/Login.cs
@@ -1,7 +1,9 @@
 ﻿using bank_app.Managers;
+using bank_app.Models.Users;
 using bank_app.Utility;
 using bank_app.Utility.UI;
 using bank_app.Utility.UI.Components;
+using bank_app.Utility.UI.Invokables;
 
 namespace bank_app.UI
 {
@@ -10,6 +12,8 @@ namespace bank_app.UI
         public static void Menu()
         {
             Console.CursorVisible = false;
+            // Create a new Invokable object with the method that is going to run once the 'Login' button is pressed
+            var loginInvoke = new Invokable<string, string>(LoginHandler);
             // Create new 3x3 grid
             Grid grid = new Grid(3, 3);
             // Add Menu with two inputfields and button inside to center middle of grid. Note: Button is currently not finished, so it won't be rendered
@@ -19,7 +23,7 @@ namespace bank_app.UI
             cell.OrderBy = OrderBy.Column;
             grid.AddGridComponent(1, 1, new Layout(new Menu(new List<UIComponent>{new InputField("Username",false,16),
                 new InputField("Password",true,16),
-                new Button(UserManager.Login, "Login")}), LayoutBorder.Rounded, "Login", 40, 10, ColourFG.Yellow));
+                new Button(loginInvoke, "Login")}), LayoutBorder.Rounded, "Login", 40, 10, ColourFG.Yellow));
             // Add text to center bottom
             cell = grid.GetGridCell(0, 1);
             cell.Justify = Justify.Center;
@@ -27,8 +31,21 @@ namespace bank_app.UI
             cell.OrderBy = OrderBy.Column;
             grid.AddGridComponent(0, 1, new AsciiArt("Assets/Ascii/bank.txt"));
             // Add grid to layout and set rounded border style
-            Layout layout = new Layout(grid, LayoutBorder.Heavy);
+            Layout layout = new(grid, LayoutBorder.Heavy);
             layout.Render();
+        }
+
+        private static void LoginHandler(string username, string password)
+        {
+            User? user = UserManager.Login(username, password);
+            if (user != null)
+            {
+                // Login successful, switch page and give feedback
+            }
+            else
+            {
+                // Login not successful, give feedback
+            }
         }
     }
 }
