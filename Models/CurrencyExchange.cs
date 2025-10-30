ff5585b542f5e7e6305ad7d683f39959a1d1102b
lskld <theo.lillieskold@gmail.com>
2025-10-30 14:53:32 +0100

    Created methods for exchanging currencies to and from SEK

--- Models/CurrencyExchange.cs
+++ Models/CurrencyExchange.cs
@@ -12,17 +12,28 @@ namespace bank_app.Models
         //Exchange rates relative to our base currency (SEK)
         private static Dictionary<Currency, decimal> _ratesToSEK = new Dictionary<Currency, decimal>
         {
-            {Currency.SEK, 1.00m },
-            {Currency.USD, 0.11m },
-            {Currency.EUR, 0.09m },
-            {Currency.GBP, 0.08m },
-            {Currency.JPY, 16.26m },
-            {Currency.AUD, 0.16m },
-            {Currency.CAD, 0.15m },
-            {Currency.CHF, 0.09m },
-            {Currency.CNY, 0.76m },
-            {Currency.NZD, 0.18m },
-            {Currency.SLC, 0.000004m }
+            {Currency.SEK, 1.00m},
+            {Currency.USD, 0.11m},
+            {Currency.EUR, 0.09m},
+            {Currency.GBP, 0.08m},
+            {Currency.JPY, 16.26m},
+            {Currency.AUD, 0.16m},
+            {Currency.CAD, 0.15m},
+            {Currency.CHF, 0.09m},
+            {Currency.CNY, 0.76m},
+            {Currency.NZD, 0.18m},
+            {Currency.SLC, 0.000004m}
         };
+
+        public static decimal ExchangeToSek(decimal amount, Currency currencyFrom, Currency sek = Currency.SEK)
+        {
+            decimal exchangeRate = _ratesToSEK[currencyFrom];
+            return amount / exchangeRate;
+        }
+        public static decimal ExchangeFromSek(decimal amount, Currency currencyTo, Currency sek = Currency.SEK)
+        {
+            decimal exchangeRate = _ratesToSEK[currencyTo];
+            return amount * exchangeRate;
+        }
     }
 }
