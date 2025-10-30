a78816d19d72be06877add50d6f438751a9e736e
Svyatoslav <slavalapshin17@gmail.com>
2025-10-30 10:47:43 +0100

    loan small changes

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bank_app.Models
+﻿namespace bank_app.Models
 {
     public class Loan
     {
         public Guid LoanId { get; }
         public Guid UserId { get; set; }
-        public decimal  Principal { get; set; }
-        public decimal  OutStandingPrincipal { get; set; }
+        public decimal Principal { get; set; }
+        public decimal OutStandingPrincipal { get; set; }
         public decimal AccruedInterest { get; set; }
-        public decimal  AnnualRate { get; set; }
-        public  DateTime LastAccrualDate { get; set; }
+        public decimal AnnualRate { get; set; }
+        public DateTime LastAccrualDate { get; set; }
 
 
 
