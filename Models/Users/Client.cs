217e109c10d78eaadde3ccd98bd631519efb09e6
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-10-31 10:26:07 +0100

    Updated Client class to have private setters and public methods to reach them

--- Models/Users/Client.cs
+++ Models/Users/Client.cs
@@ -10,8 +10,8 @@ namespace bank_app.Models.Users
 {
     public class Client : User
     {
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string? Email { get; private set; }
+        public string? PhoneNumber { get; private set; }
         public List<Account> MyAccounts { get; set; } = new List<Account>();
 
         public Client(string userName, string userPassword, string email, string phoneNumber) 
@@ -20,5 +20,34 @@ namespace bank_app.Models.Users
             Email = email;
             PhoneNumber = phoneNumber;
         }
+
+
+        // private setters
+        private void setEmail(string email)
+        {
+            Email = email;
+        }
+
+        private void setPhoneNumber(string phoneNumber)
+        {
+            PhoneNumber = phoneNumber;
+        }
+
+        // public methods to reach private setters
+
+        public void updateContactInfo(string type, string change)
+        {
+            switch (type)
+            {
+                case "email":
+                    setEmail(change);
+                    break;
+
+                case "number":
+                    setPhoneNumber(change);
+                    break;
+            }
+        }
     }
+
 }
