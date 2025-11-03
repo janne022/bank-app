0171cc2e448f6d565c484060dbf6fb678ef1f741
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-03 16:41:23 +0100

    added missing argument to createUser

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ internal class Program
         // Starts the async method inside a background thread that needs to run without getting blocked from ex. UI
         Task.Run(BackgroundThread);
 
-        UserManager.CreateUser("admin", "admin", UserType.Admin, "admin@gmail.com", "0709431135");
+        UserManager.CreateUser("admin", "admin", UserType.Admin, "admin@gmail.com", "0709431135", "Creator");
 
         // Start the first User Interface page
         PageManager.Start(PageType.Login);
