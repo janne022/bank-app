95c1ccf4ae10b5e564c059c20ce79f86defa8e36
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-04 11:34:15 +0100

    started refactoring/cleanup

--- Managers/UserManager.cs
+++ Managers/UserManager.cs
@@ -9,10 +9,13 @@ namespace bank_app.Managers
         public static List<string> UserIds { get; } = new List<string>();
 
         /// <summary>
-        /// Method to create new users within the bank app. Adds the newly created user to a list of all users within the application.
+        ///  Creates a user object with the inputted arguments
         /// </summary>
-        /// <param name="userType">Enum that determines whether the user is a client or an administrator</param>
-
+        /// <param name="userId">must be a unique login name</param>
+        /// <param name="email">must be correctly formatted with @</param>
+        /// <param name="phoneNumber">must be correctly formatted with country code and correct amount of digits</param>
+        /// <param name="legalName">The individual's legal first name</param>
+        /// <returns></returns>
         internal static User CreateUser(string userId, string userPassword, UserType userType, string email, string phoneNumber, string legalName)
         {
             if (userType is UserType.Admin)
@@ -31,10 +34,6 @@ namespace bank_app.Managers
             return null;
         }
 
-        /// <summary>
-        /// Removes user object from the user list, and nullifies the references of that specific object.
-        /// </summary>
-        /// <param name="userName">Name of the user object to be removed</param>
         internal static void RemoveUser(User userName)
         {
             Users.Remove(userName);
@@ -53,9 +52,6 @@ namespace bank_app.Managers
             return null;
         }
 
-        /// <summary>
-        /// Attempts to authorize a user by verifying the provided password.
-        /// </summary>
         internal static bool Authorization(string userID, string inputPassword)
         {
             var user = GetUser(userID);
@@ -136,37 +132,16 @@ namespace bank_app.Managers
                 {
                     return null;
                 }
-
             }
-
             // ...and if no user matches the name, password or the user is not authorized to login: return nothing.
             return null;
         }
 
-
-        ////New Method to Login user with User Id
-        //internal static User? Login(string userId, string password)
-        //{
-
-        //    var user = Users.FirstOrDefault(u => u.UserId.ToString() == userId && Authorization(u.UserId.ToString(), password));
-
-        //    if (user == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    return user;
-        //}
         internal static void Logout()
         {
             // UI needed to develop - change page to login page
         }
 
-
-        /// <summary>
-        /// Updates the properties of the user object
-        /// </summary>
-        /// <param name="user">the name of the user object to be changed</param>
         public static void ChangeUserInfo(User user, string typeOfChange, string change)
         {
             user.UpdateUserInfo(typeOfChange, change);
