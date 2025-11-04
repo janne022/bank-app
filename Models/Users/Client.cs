77a308302a635e603e2d4bb48948280fc5fa89cc
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-04 12:08:40 +0100

    removed unessesary logic

--- Models/Users/Client.cs
+++ Models/Users/Client.cs
@@ -10,43 +10,11 @@ namespace bank_app.Models.Users
 {
     public class Client : User
     {
-        public string? Email { get; private set; }
-        public string? PhoneNumber { get; private set; }
         public List<Account> MyAccounts { get; } = new List<Account>();
 
         public Client(string userName, string userPassword, string email, string phoneNumber, string legalName)
             : base(userName, userPassword, email, phoneNumber, legalName)
         {
-            Email = email;
-            PhoneNumber = phoneNumber;
-        }
-
-
-        // private setters
-        private void SetEmail(string email)
-        {
-            Email = email;
-        }
-
-        private void SetPhoneNumber(string phoneNumber)
-        {
-            PhoneNumber = phoneNumber;
-        }
-
-        // public methods to reach private setters
-
-        public void UpdateContactInfo(string type, string change)
-        {
-            switch (type)
-            {
-                case "email":
-                    SetEmail(change);
-                    break;
-
-                case "number":
-                    SetPhoneNumber(change);
-                    break;
-            }
         }
     }
 
