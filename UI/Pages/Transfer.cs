d45dae82cf1752a263b9b61a2d48a1cf299c6f9c
Johannes Flodin <jannis020609@gmail.com>
2025-11-04 14:38:39 +0100

    added account list

--- UI/Pages/Transfer.cs
+++ UI/Pages/Transfer.cs
@@ -1,4 +1,6 @@
-﻿using bank_app.Utility;
+﻿using bank_app.Managers;
+using bank_app.Models.Accounts;
+using bank_app.Utility;
 using bank_app.Utility.Components;
 using bank_app.Utility.UI;
 using bank_app.Utility.UI.Components;
@@ -16,12 +18,13 @@ namespace bank_app.UI.Pages
         internal override UIComponent LoadPage()
         {
             Console.CursorVisible = false;
+            List<Models.Accounts.Account> accounts = AccountManager.GetAllAccounts(CurrentUser.UserId).Where(account => account is CheckingAccount || account is SavingsAccount).ToList();
             var transferInvokable = new Invokable<string, string>(TransferMoney);
             var navbar = new Navbar(new List<NavbarItem> { new NavbarItem("Home", PageType.ClientDashboard), new NavbarItem("Transfer", PageType.Transfer), new NavbarItem("Transaction", PageType.Transaction), new NavbarItem("Account", PageType.Account), new NavbarItem("Loan", PageType.Loan) });
             // Create a new Invokable object with the method that is going to run once the 'Login' button is pressed
             var transferGrid = new Flexbox(Justify.Center, Align.Top);
             transferGrid.AddFlexComponent(new Text($"Avaliable balance: "));
-            transferGrid.AddFlexComponent(new Form(new List<UIComponent> { new InputField("Account ID", false, 24), new InputField("Amount", false, 24) }, new Button(transferInvokable, "Send")));
+            transferGrid.AddFlexComponent(new Form(new List<UIComponent> {new InputField("Account ID", false, 24), new InputField("Amount", false, 24) }, new Button(transferInvokable, "Send")));
             transferGrid.Width = 40;
             transferGrid.Height = 10;
             // Create new 3x3 grid
