7c7eb9a411632b902c86bbb9dc3a8eeb5e697585
lskld <theo.lillieskold@gmail.com>
2025-11-04 09:28:31 +0100

    Fixed Admin dashboard + subpages

--- UI/Pages/UpdateRates.cs
+++ UI/Pages/UpdateRates.cs
@@ -13,21 +13,18 @@ using System.Threading.Tasks;
 
 namespace bank_app.UI.Pages
 {
-    public class AdminDashboard : Page
+    internal class UpdateRates : Page
     {
-
         internal override UIComponent LoadPage()
         {
-            var user = PageManager.GetCurrentUser();
-
             Console.CursorVisible = false;
-            var navbar = new Navbar(new List<NavbarItem> { new NavbarItem("Home", PageType.AdminDashboard), new NavbarItem("Create User", PageType.CreateUser), 
+            var navbar = new Navbar(new List<NavbarItem> { new NavbarItem("Home", PageType.AdminDashboard), new NavbarItem("Create User", PageType.CreateUser),
                 new NavbarItem("Check Transactions", PageType.CheckTransactions), new NavbarItem("Update Rates", PageType.UpdateRates)});
             // Create a new Invokable object with the method that is going to run once the 'Login' button is pressed
             // Create new 3x3 grid
             Grid grid = new Grid(3, 3);
             grid.AddGridComponent(0, 1, navbar);
-            grid.AddGridComponent(1, 1, new Text($"Welcome back {user?.UserName}!"));
+            grid.AddGridComponent(1, 1, new Text("Home"));
             // Add grid to layout and set rounded border style
             return new Panel(grid, LayoutBorder.Heavy);
         }
