a73f13dad20ad2ae8d0c8f404c9a8130b2fb30f8
Johannes Flodin <jannis020609@gmail.com>
2025-11-04 15:11:24 +0100

    fixed flexbox align middle issue and added Transfer page

--- UI/Pages/Transfer.cs
+++ UI/Pages/Transfer.cs
@@ -18,26 +18,30 @@ namespace bank_app.UI.Pages
         internal override UIComponent LoadPage()
         {
             Console.CursorVisible = false;
-            List<Models.Accounts.Account> accounts = AccountManager.GetAllAccounts(CurrentUser.UserId).Where(account => account is CheckingAccount || account is SavingsAccount).ToList();
-            var transferInvokable = new Invokable<string, string>(TransferMoney);
+            // Gets all checking accounts and saving accounts, then returns them as a DropdownItem with account id as name
+            List<DropdownItem<Models.Accounts.Account>> accounts = AccountManager.GetAllAccounts(CurrentUser.UserId).Where(account => account is CheckingAccount || account is SavingsAccount).Select(account => new DropdownItem<Models.Accounts.Account>(account.AccountID.ToString(), account)).ToList();
+            var transferInvokable = new Invokable<Models.Accounts.Account,string, string>(TransferMoney);
             var navbar = new Navbar(new List<NavbarItem> { new NavbarItem("Home", PageType.ClientDashboard), new NavbarItem("Transfer", PageType.Transfer), new NavbarItem("Transaction", PageType.Transaction), new NavbarItem("Account", PageType.Account), new NavbarItem("Loan", PageType.Loan) });
             // Create a new Invokable object with the method that is going to run once the 'Login' button is pressed
             var transferGrid = new Flexbox(Justify.Center, Align.Top);
             transferGrid.AddFlexComponent(new Text($"Avaliable balance: "));
-            transferGrid.AddFlexComponent(new Form(new List<UIComponent> {new InputField("Account ID", false, 24), new InputField("Amount", false, 24) }, new Button(transferInvokable, "Send")));
+            transferGrid.AddFlexComponent(new Form(new List<UIComponent> {new Dropdown<Models.Accounts.Account>(accounts), new InputField("Account ID", false, 34), new InputField("Amount", false, 24) }, new Button(transferInvokable, "Send")));
             transferGrid.Width = 40;
             transferGrid.Height = 10;
             // Create new 3x3 grid
             Grid grid = new Grid(3, 3);
             grid.AddGridComponent(0, 1, navbar);
-            grid.AddGridComponent(1,1, new Panel(transferGrid, LayoutBorder.Rounded,width: 40, height: 10));
+            grid.AddGridComponent(1,1, new Panel(transferGrid, LayoutBorder.Rounded,width: 70, height: 15));
+            var gridCell = grid.GetGridCell(1,1);
+            gridCell.Justify = Justify.Center;
+            gridCell.Align = Align.Middle;
             // Add grid to layout and set rounded border style
             return new Panel(grid, LayoutBorder.Heavy);
         }
 
-        private void TransferMoney(string id, string amount)
+        private void TransferMoney(Models.Accounts.Account account,string id, string amount)
         {
-
+            Console.WriteLine($"{account.Balance} {id} {amount}");
         }
     }
 }
