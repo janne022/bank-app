4fa96602ae704f07cf73a0fa010a90aa65f60f0c
lskld <theo.lillieskold@gmail.com>
2025-11-05 17:23:42 +0100

    Cleanup AccountDefaults.cs

--- Models/Accounts/AccountDefaults.cs
+++ Models/Accounts/AccountDefaults.cs
@@ -1,28 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bank_app.Models.Accounts
+﻿namespace bank_app.Models.Accounts
 {
     public static class AccountDefaults
     {
         //This class holds default values for different account types
-        // Checking
 
-
-
+        //Checking Account
         public const decimal CheckingMonthlyFee = 10m;
         public const decimal CheckingOverdraftLimit = 500m;
 
-        // Savings
+        //Savings Account
         public const decimal SavingsInterestRate = 5m;
         public const decimal SavingsMinimumBalance = 100m;
         public const bool SavingsAllowWithdraws = true;
 
-
-        // Loan
+        //Loan Account
         public const decimal LoanInterestRate = 7m;
         public const decimal LoanCreditLimit = 10000000m;
         public const int MaxLoansPerUser = 5;
