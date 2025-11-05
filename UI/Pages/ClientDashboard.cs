ac9db9e76926827b70e34293da531f7988def1a8
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-11-05 15:58:16 +0100

    Added a feed of the users accounts, started fixing with border around the accounts. Need update from Emma to be able to move the feed into the border

--- UI/Pages/ClientDashboard.cs
+++ UI/Pages/ClientDashboard.cs
@@ -34,6 +34,36 @@ namespace bank_app.UI.Pages
                 .SetJustify(Justify.Center)
                 .SetAlign(Align.Top);
             grid.AddGridComponent(0, 1, new AsciiArt(AsciiType.String, FiggleFonts.Small.Render($"Welcome back {CurrentUser?.LegalName}")));
+
+
+
+
+            // creates a table of account information
+            var accountFeed = new Feed(AccountManager.GetAllAccounts(CurrentUser.UserId).Count);
+            string[] column1 = new string[AccountManager.GetAllAccounts(CurrentUser.UserId).Count];
+            string[] column2 = new string[AccountManager.GetAllAccounts(CurrentUser.UserId).Count];
+
+            // loops as many times as there are accounts...
+            for (int i = 0; i < AccountManager.GetAllAccounts(CurrentUser.UserId).Count; i++)
+            {
+                // adds info about the accounts first in first column, then second...
+                column1[i] = $"Account {i + 1} <{AccountManager.GetAllAccounts(CurrentUser.UserId)[i].GetType().Name.Substring(0, AccountManager.GetAllAccounts(CurrentUser.UserId)[i].GetType().Name.Length - 7)}>: ";
+                column2[i] = $"{AccountManager.GetAllAccounts(CurrentUser.UserId)[i].Balance} {AccountManager.GetAllAccounts(CurrentUser.UserId)[i].AccountCurrency}";
+            }
+
+            // ... then adds those columns into a left and right column...
+            FeedColumn leftColumn = new FeedColumn(column1, "Vira was here >:3", false, false);
+            FeedColumn rightColumn = new FeedColumn(column2, "Vira was also here, hehe c:", false, false);
+
+            //...and finally adds the columns into the table
+            accountFeed.AddColumn(leftColumn);
+            accountFeed.AddColumn(rightColumn);
+            grid.AddGridComponent(1, 1, accountFeed);
+            grid.AddGridComponent(0, 1, new Panel(accountFeed, LayoutBorder.Rounded, "Accounts", 100, 15, ColourFG.GreenBright));
+
+
+
+
             // Add grid to layout and set rounded border style
             return new Panel(grid, LayoutBorder.Heavy);
         }
