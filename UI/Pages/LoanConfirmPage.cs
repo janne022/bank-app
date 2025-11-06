3c7ab809442e10ae9a41b0cc7fc9b920c1f684af
lskld <theo.lillieskold@gmail.com>
2025-11-06 11:17:49 +0100

    Base layout for LoanConfirmationPage

--- UI/Pages/LoanConfirmPage.cs
+++ UI/Pages/LoanConfirmPage.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using bank_app.Models.Accounts;
+using bank_app.Utility;
+using bank_app.Utility.Components;
+using bank_app.Utility.UI;
+using bank_app.Utility.UI.Components;
+using bank_app.Utility.UI.Invokables;
+using Figgle.Fonts;
 
 namespace bank_app.UI.Pages
 {
     internal class LoanConfirmPage : Page
     {
+        internal override UIComponent LoadPage()
+        {
+            Console.CursorVisible = false;
 
+            var navbar = new Navbar(
+                [
+                    new("Continue", PageType.ClientDashboard),
+                ]);
+
+            var continueInvoke = new Invokable(NavigationHandler);
+
+            Grid grid = new(3, 3);
+            grid.AddGridComponent(0, 1, navbar)
+                .SetJustify(Justify.Center)
+                .SetAlign(Align.Top);
+            grid.AddGridComponent(0, 1, new AsciiArt(AsciiType.String, FiggleFonts.Small.Render("Loan Confirmed")));
+
+            grid.AddGridComponent(1, 1, new Text($"Your interest is: {AccountDefaults.LoanInterestRate}%"));
+            grid.AddGridComponent(1, 1, new Text("Your monthly payment is: "));
+
+            return new Panel(grid, LayoutBorder.Heavy);
+        }
+
+        private void NavigationHandler()
+        {
+            PageManager.SwitchPage(PageType.ClientDashboard);
+        }
     }
 }
